fce676bbf4872cb40094d55ad5515e570d993f8e
KentaWakabayashi <kenken050628@gmail.com>
2021-02-08 07:27:12 +0000

    secondcommit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9d2cafcd87c948ac844951952fcd1a77625e8abd5736c3df2a54deaf78acb740127a56f7f9d081afad3a0bb18f0c7a7705eafdb4edae6ffd09e184ef857a4244'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'tops#top'
+  get 'top/about' => 'tops#about'
+  get "users/withdrawal" => "users#withdrawal"
+  patch "users/withdrawal" => "users#withdrawal"
+
+  resources :users,only: [:new, :show, :index, :edit, :destroy, :update] do
+  resource :relationships, only:[:create, :destroy]
+    get 'follower' => 'relationships#follower', as: 'follower'
+    get 'followed' => 'relationships#followed', as: 'followed'
+
+  end
+   resources :posts do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+
+  resources :post, only: [:new, :create, :index, :show, :edit, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
+    resource :relationship,only: [:create, :destroy, :followings, :followers]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+  get 'search' => 'searchs#search', as: 'search'
+
 end
